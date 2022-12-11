8750c432cc4615d37a1ddb3a80ff837c6ade7f5b
SuperSonic <supersonic@livemail.tw>
2022-12-11 16:02:45 +0800

    Update

--- main/caos_client.c
+++ main/caos_client.c
@@ -2,12 +2,14 @@
 // (c) 2022 Star Inc. (https://starinc.xyz)
 // License: MIT
 
-#define CAOS_CLIENT_HOST "http://example.org"
-#define CAOS_CLIENT_PORT 80
+#define CAOS_CLIENT_HOST "https://client.startw.cf"
+#define CAOS_CLIENT_PORT 443
+#define CAOS_CLIENT_PATH_PREFIX "/metastar_caos/cloud"
 
 #define CAOS_CLIENT_USER_AGENT "startw-caos device v2.0"
 
 #include "esp_http_client.h"
+#include "esp_log.h"
 
 #include "wifi_sta.h"
 #include "caos_client.h"
@@ -28,6 +30,10 @@ int caos_client_get_state()
     return CAOS_CLIENT_STATE_ONLINE;
 }
 
+esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
+    return ESP_OK;
+};
+
 /**
  * caos_client_get_device_profile
  * returns the profile of this device.
@@ -37,7 +43,8 @@ caos_client_device_profile_t *caos_client_get_device_profile()
     caos_client_device_profile_t *profile;
 
     esp_http_client_config_t config = {
-        .host = "httpbin.org",
+        .host = CAOS_CLIENT_HOST,
+        .port = CAOS_CLIENT_PORT,
         .path = "/get",
         .transport_type = HTTP_TRANSPORT_OVER_TCP,
         .event_handler = _http_event_handler,
@@ -49,14 +56,11 @@ caos_client_device_profile_t *caos_client_get_device_profile()
         ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
                 esp_http_client_get_status_code(client),
                 esp_http_client_get_content_length(client));
-
-        // Parse JSON
-        body = esp_http_client_read(client);
-
-        profile->device_name = "ESP32";
     } else {
         ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
     }
+
+    profile = malloc(sizeof(caos_client_device_profile_t));
 
     return profile;
 }
